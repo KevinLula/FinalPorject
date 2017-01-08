81264231c6b853f7ba730dbefcebce969cec8ec6
KevinLula <kevinlula2014@gmail.com>
2017-01-07 20:08:29 -0500

    New Buttons... have to change grid

--- GUI/Buttons.java
+++ GUI/Buttons.java
@@ -5,10 +5,11 @@ import javax.swing.border.*;
 
 public class Buttons extends JPanel {
     public Buttons(){
-        this.setLayout(new GridLayout(5,1));
+        this.setLayout(new GridLayout(4,1));
         this.add(new JButton("Reset"));
-        this.add(new JButton("Solve"));
+        this.add(new JButton("Check"));
         this.add(new JButton("New Puzzle"));
+	this.add(new JButton("Give Up"));
          
     }
 }
