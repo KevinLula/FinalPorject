03fdb288110aa8b60ca2f0a60dede47afd1ec8b6
KevinLula <kevinlula2014@gmail.com>
2017-01-07 20:12:25 -0500

    New cell object used to make a box 3x3 filled with empty text fields. Border colored to seperate from future boxes

---
+++ GUI/Box.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;//NEW STUFF!
+import java.awt.event.*;
+import javax.swing.border.*;
+
+public class Box extends JPanel {
+    public int CellNumber = 9;
+     
+    public Cell[]cells = new Cell[CellNumber];
+     
+    public Box(){
+        this.setLayout(new GridLayout(3,3));
+        this.setBorder(new LineBorder(Color.BLACK));
+        for(int i = 0; i < CellNumber; i++){
+            cells[i] = new Cell();
+            this.add(cells[i]);
+        }
+    }
+}
