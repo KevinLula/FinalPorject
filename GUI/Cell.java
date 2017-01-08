cff8521f474fd0793f17d6010624b62110e86b16
KevinLula <kevinlula2014@gmail.com>
2017-01-07 20:09:47 -0500

    The text fields where the numbers will be put. will be set as uneditable later

---
+++ GUI/Cell.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;//NEW STUFF!
+import java.awt.event.*;
+import javax.swing.border.*;
+
+public class Cell extends JTextField {
+    public int number;
+    public Cell() {  }
+}
